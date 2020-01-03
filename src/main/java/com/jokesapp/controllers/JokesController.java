c77ba968fa66ac4f9167b6b8b10bb18270227083
AhsanFaheem <ahsanfaheem1234@gmail.com>
2020-01-03 11:27:06 +0500

    Refactored by using Constructor based injection for jokes service in controller

--- src/main/java/com/jokesapp/controllers/JokesController.java
+++ src/main/java/com/jokesapp/controllers/JokesController.java
@@ -1,27 +1,28 @@
 package com.jokesapp.controllers;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.jokesapp.model.Joke;
 import com.jokesapp.services.JokesService;
 
 @Controller
 public class JokesController {
 
-	@Autowired
 	JokesService JokesService;
-	
-	@RequestMapping(value="/")
+
+	@RequestMapping(value = "/")
 	public String index(Model model) {
-		
-		model.addAttribute("joke",JokesService.obtainJoke());
+
+		model.addAttribute("joke", JokesService.obtainJoke());
 		return "chucknorris";
 	}
-	
-	
+
+	@Autowired
+	public JokesController(JokesService jokesService) {
+		super();
+		JokesService = jokesService;
+	}
+
 }
